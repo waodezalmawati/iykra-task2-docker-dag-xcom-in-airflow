27f84c8ee06c73348ce38e016a981350e869e3d7
waodezalmawati <you@example.com>
2022-09-24 23:31:27 +0800

    upload file

---
+++ dags/task2_xcom_dag.py
@@ -0,0 +1,64 @@
+from sched import scheduler
+from airflow import DAG
+#import operator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+
+from random import randint
+from datetime import datetime
+
+def _choose_best_model(ti):
+    accuracies = ti.xcom_pull(key='model_accuracy', task_ids=[
+        'training_model_A',
+        'training_model_B',
+        'training_model_C'
+    ])
+    best_accuracy=max(accuracies)
+    if (best_accuracy > 8):
+        return "accurate"
+    return "inaccurate"
+
+def _training_model(ti):
+    # return randint(1,10)
+    accuracy = randint(1,10)
+    print(f'model\'s accuracy: {accuracy}')
+    ti.xcom_push(key='model_accuracy', value=accuracy)
+
+with DAG("my_dag", start_date=datetime(2022, 1, 1), schedule_interval="@daily", catchup=False) as dag:
+    #Buat Task 1
+    training_model_A = PythonOperator(
+        task_id="training_model_A",
+        python_callable=_training_model
+     )
+      
+    #Buat Task 2
+    training_model_B = PythonOperator(
+        task_id="training_model_B",
+        python_callable=_training_model
+     )
+      
+    #Buat Task 3
+    training_model_C = PythonOperator(
+        task_id="training_model_C",
+        python_callable=_training_model
+     )
+
+    #Buat Task pilih best akurasi
+    choose_best_model = BranchPythonOperator(
+        task_id="choose_best_model",
+        python_callable=_choose_best_model
+    )
+
+    #accuracy or in
+    accurate = BashOperator(
+        task_id="accurate",
+        bash_command="echo 'accurate'"
+    )
+    
+    inaccurate = BashOperator(
+        task_id="inaccurate",
+        bash_command="echo 'inaccurate'"
+    )
+
+#Set Dependencies
+[training_model_A, training_model_B, training_model_C] >> choose_best_model >> [accurate, inaccurate]
